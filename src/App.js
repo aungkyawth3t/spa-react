b8aeedac74189cd6b7920733811bd8b54ec5b079
aungkyawthetz <aungkyawthethimself@gmail.com>
2025-07-21 21:32:41 +0630

    two ways to using image in jsx

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import Image from "./assets/image.jpg";
 
 function App() {
+  let name = "Aung Kyaw Thet";
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Hello {name} </h1>
+      {/* <img src="/image.jpg" alt="" /> */}
+      <img src={Image} alt="" />
     </div>
   );
 }
