4f29ba759276677a0d80e5fa56a73cc85527516e
aungkyawthetz <aungkyawthethimself@gmail.com>
2025-07-21 22:01:09 +0630

    useState

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
-import Image from "./assets/image.jpg";
+import { useState } from 'react';
 
+// hook useState
 function App() {
-  let name = "Aung Kyaw Thet";
+  // let name = "Aung Kyaw Thet";
+  let [name, setName] = useState("Aung Kyaw Thet") // [getter, setter]
+
+  let changeName = () => {
+    setName('Aung Aung');
+    console.log(name);
+  }
+
   return (
     <div className='app'>
       <h1>Hello {name} </h1>
+      <button onClick={changeName}>Change Name</button>
     </div>
   );
 }
