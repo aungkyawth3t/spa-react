19220b43956225dbc291683d4b220e3a0829acaf
aungkyawth3t <aungkyawthethimself@gmail.com>
2025-07-25 20:56:20 +0630

    useRef

--- src/components/PostForm/index.js
+++ src/components/PostForm/index.js
@@ -1,18 +1,20 @@
-import React, { useState } from 'react'
+import React, { useRef, useState } from 'react'
 import './index.css';
 
 export default function PostForm({ addPost }) {
 
-  let [title, setTitle] = useState("");
+  // let [title, setTitle] = useState("");
+  let title = useRef();
   let resetForm = () => {
-    setTitle('');
+    // setTitle('');
+    title.current.value = '';
   }
 
   let uploadPost = (e) => {
     e.preventDefault();
     let post = {
       id: Math.floor(Math.random() * 10000),
-      title: title
+      title: title.current.value
     }
     addPost(post);
     resetForm();
@@ -22,7 +24,7 @@ export default function PostForm({ addPost }) {
       <h1>Create Post</h1>
       <div className="form-control">
         <label htmlFor="">Title</label>
-        <input type="text" onChange={(e) => setTitle(e.target.value)} value={title} />
+        <input type="text" ref={title} />
       </div>
       <div className="form-control">
         <button>Post</button>
